fe5294a4374e796c496330870fc618a61ba9bc77
Germano <gmanopoliano@gmail.com>
2016-03-14 13:49:11 -0300

    Adição da pagina de cadastro

--- exercitando/projetoASUS/crudAluno/urls.py
+++ exercitando/projetoASUS/crudAluno/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
-    url(r'^lista_pessoas/$', lista_pessoas, name='namespace_lista_pessoas'),
+    url(r'^$', lista_pessoas, name='namespace_lista_pessoas'),
+    url(r'^cadastrar_pessoa/', cadastrar_pessoa, name='namespace_cadastrar_pessoa'),
 ]
--- exercitando/projetoASUS/crudAluno/views.py
+++ exercitando/projetoASUS/crudAluno/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 from .models import Pessoa, Aluno
@@ -10,3 +10,14 @@ def lista_pessoas(request):
 		"pessoas":pessoas
 	}
 	return render(request, 'lista_pessoas.html', context)
+
+def cadastrar_pessoa(request):
+	form = PessoaForm(request.POST or None)
+	if form.is_valid():
+		pessoa = Pessoa()
+		pessoa = form.save(commit=False)
+		pessoa.save()
+	context = {
+		'form' : form
+	}
+	return render(request, 'cadastrar_pessoa.html', context)
